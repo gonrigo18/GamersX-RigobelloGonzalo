12d3a0059711310d7c7c4e3782bccec4afce758b
Gonzalo Rigobello <103467918+gonrigo18@users.noreply.github.com>
2022-08-26 09:35:18 -0300

    Version 2.1

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import Contador from "./components/Contador"
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function App() {
-
+const App = () => {
 
   return (
+
     <div>
       <Navbar/>
-      <ItemListContainer greeting="propiedad" />
       <Contador/>
+      <ItemListContainer/>
     </div>
   );
 }
+
+export default App;
